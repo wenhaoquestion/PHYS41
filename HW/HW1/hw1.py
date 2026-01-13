f798fd2459e204a59c74ebb91cdda88e109c0532
wenhaoquestion <93458283+wenhaoquestion@users.noreply.github.com>
2026-01-13 09:42:54 -0800

    Initial commit

---
+++ HW/HW1/hw1.py
@@ -0,0 +1,35 @@
+my_fav_number=3.141592653589793
+import numpy as np
+
+def solution_a(list_in,name_in):
+    count=0
+    for item in list_in:
+        count+=(item==name_in)
+        
+    return count
+    
+def solution_b(list_in):
+    sumtot=0
+    prod=1
+    for item in list_in:
+        sumtot+=item
+        prod*=item
+    return sumtot,prod
+    
+
+
+def mysort(a):
+    for n in range(1,len(a)):
+        #Read in one element at a time, starting with second (first is sorted)
+        value=a[n]
+        #set marker for previous
+        i=n-1
+        while i>=0 and (value > a[i]):
+            #if i is not past the first element, but value > previous element swith the two
+            a[i+1]=a[i]
+            a[i] = value
+            #now move the marker one to the left and repeat
+            i-=1
+#1st element is sorted.  When we get to the nth element, the n-1 previous elements are sorted.  Just need to place it in the right spot
+
+
